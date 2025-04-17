1e763eb57ae2d21b7b35f1475c9c55ffeac4dbed
Hanglesson <hanglesson.dev@gmail.com>
2025-04-16 22:06:16 -0500

    feat: add PWA basic support with Vite PWA module and launcher icons
    - Added @vite-pwa/nuxt dependency to package.json for PWA functionality.
    - Included various Android launcher icons (48x48, 72x72, 96x96, 144x144, 192x192, 512x512) in the public/icons/pwa directory.

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import { NuxtPwaManifest } from '#components';
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <!-- <VitePwaManifest /> -->
+    <NuxtPwaManifest />
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,60 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+    '@nuxt/test-utils',
+    '@vite-pwa/nuxt',
+  ],
+  pwa: {
+    manifest: {
+      name: 'Meu Planejador de Rotas',
+      short_name: 'Planejador de Rotas',
+      description: 'Um aplicativo para planejar suas rotas de entregas.',
+      start_url: '/',
+      display: 'standalone',
+      // background_color: '#ffffff', // Cor de fundo da splash screen
+      // theme_color: '#4DBA87', // Cor da barra de status (deve combinar com o tema do app)
+      orientation: 'any',
+      lang: 'pt-BR',
+      icons: [
+        {
+          src: 'icons/pwa/android-launchericon-512-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/pwa/android-launchericon-192-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/pwa/android-launchericon-144-144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/pwa/android-launchericon-96-96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/pwa/android-launchericon-72-72.png',
+          sizes: '72x72',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/pwa/android-launchericon-48-48.png',
+          sizes: '48x48',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+      suppressWarnings: true,
+    },
+  },
+});
